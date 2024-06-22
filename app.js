13894c8af154574ec3556f3d078c1d2eb5088fd5
Wasi khan <wasikhan7099@gmail.com>
2024-06-22 01:11:56 -0700

    modal

---
+++ app.js
@@ -0,0 +1,10 @@
+const modale = document.querySelector('.modale');
+const close = document.querySelector('.close');
+
+function modalHandler() {
+    modale.classList.add('show');
+}
+
+function closeHandler() {
+    modale.classList.remove('show');
+}
